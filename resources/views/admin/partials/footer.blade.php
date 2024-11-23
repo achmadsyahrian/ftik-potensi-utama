7e34c9fad493b26af6acbe38493540c33fe31db4
Achmad Syahrian <achmadsyahrian1@gmail.com>
2024-11-23 11:11:54 +0700

    add manual book

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -10,7 +10,7 @@
                 <ul class="list-inline list-inline-dots mb-0">
                     <li class="list-inline-item">
                         Copyright &copy; 2024
-                        <a href="." class="link-secondary">Achmad Syahrian</a>.
+                        <a href="http://potensi-utama.ac.id/" class="link-secondary">Universitas Potensi Utama</a>.
                         All rights reserved.
                     </li>
                 </ul>
